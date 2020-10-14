4ba1d1b567522e2d2a0ad37f4bdbb622c3344f2e
UNIVERSAL CODER <63962758+aaryanrlondhe@users.noreply.github.com>
2020-10-14 07:29:28 +0530

    Update musicplayer.py

--- musicplayer.py
+++ musicplayer.py
@@ -4,7 +4,7 @@ from pygame import mixer
 root = Tk()
 class MusicPlayer:
     root.configure(bg='#fcfcec')
-    root.geometry("470x150")
+    #root.geometry("470x150")
     def __init__(self, window ):
         def des():
             mixer.music.stop()
@@ -13,6 +13,7 @@ class MusicPlayer:
             Pause.destroy()
             Stop.destroy()
             Close.destroy()
+            root.destroy()
 
         window.title('Universal Music Player'); window.resizable(0,0)
         Load = Button(window, text = 'Load',  width = 10, command = self.load,bg='#4b7fa4',fg="#fcfcec",font=("times", 20))
@@ -20,7 +21,7 @@ class MusicPlayer:
         Pause = Button(window,text = 'Pause',  width = 10, command = self.pause,bg='#4b7fa4',fg="#fcfcec",font=("times", 20))
         Stop = Button(window ,text = 'Stop',  width = 10, command = self.stop,bg='#cb464e',fg="#fcfcec",font=("times", 20))
         Close = Button(window ,text = 'Close',  width = 10, command = des,bg='#cb464e',fg="#fcfcec",font=("times", 20))
-        Load.place(x=0,y=20);Play.place(x=150,y=20);Pause.place(x=300,y=20);Close.place(x=150,y=75)
+        Load.place(x=900,y=20);Play.place(x=1050,y=20);Pause.place(x=1200,y=20);Close.place(x=1050,y=75)
         self.music_file = False
         self.playing_state = False
     def load(self):
@@ -39,5 +40,8 @@ class MusicPlayer:
             self.playing_state = False
     def stop(self):
         mixer.music.stop()
-app= MusicPlayer(root)        
-root.mainloop()
+#app= MusicPlayer(root)        
+
+#root.mainloop()
+
+#app= MusicPlayer(root)        
